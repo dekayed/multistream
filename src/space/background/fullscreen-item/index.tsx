8f425ca5e98e487d1ae70fb7441bf55e858ad8fc
Danny Korolev <daniil.korolev@jetrockets.com>
2024-07-20 21:01:42 +0400

    fix fullscreen ctx item

--- src/space/background/fullscreen-item/index.tsx
+++ src/space/background/fullscreen-item/index.tsx
@@ -1,16 +1,7 @@
 import { ContextMenuItem } from "components/ui/context-menu";
-import { useEffect, useState } from "react";
 
 export function FullscreenItem() {
-  const [fullscreenElement, setFullscreenElement] = useState<HTMLElement | null>(null);
-
-  useEffect(() => {
-    document.addEventListener('fullscreenchange', () => {
-      setFullscreenElement(document.fullscreenElement as HTMLElement);
-    });
-  }, []);
-
-  if (fullscreenElement === document.documentElement) {
+  if (document.fullscreenElement === document.documentElement) {
     return (
       <ContextMenuItem inset onClick={() => document.exitFullscreen()}>
         Exit Fullscreen
